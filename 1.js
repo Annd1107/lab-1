f00ccfab44c267f0b823d6d092c7c65aa043bdcc
Annd1107 <anandaltankhuyag037@gmail.com>
2023-09-03 08:03:17 +0800

    lab 1

---
+++ 1.js
@@ -0,0 +1,49 @@
+console.log("ene bol console");
+var name = "Anand";
+console.log("nmg "+name+" gdg");
+
+var a, b, c;
+a=33;
+b=77;
+c=a+b;
+console.log(c);
+console.log(a+b);
+
+var x ,y ,z ,m ,isTrue = true;
+x="Anand";
+y=15 ;
+z="Nest";
+console.log("Nmg "+x+" gdg. Bi "+y+"n nastai. Bi "+z+"d surdag. Bi eregtei gedg n "+isTrue+".");
+
+var idh, dsj=null;
+console.log(idh);
+console.log(dsj);
+
+//typeof ___--> ugugdliin turliig shalgah function
+console.log(typeof a);
+console.log(typeof x);
+console.log(typeof idh);
+console.log(typeof dsj);
+
+// Math.floor(3.14) -> 3   --> butarhai toog buhel bolgon huvirgah
+console.log(Math.floor(4.1));
+
+
+
+console.log("undaanii une :" +(2000+500));
+
+var birthYear, nowAge = 15, currentYear=2023;
+birthYear=currentYear-nowAge;
+console.log("tanii tursun on bol "+birthYear);
+// Tanii 10n jiliin daraah nasaa tootsoolno uu.
+console.log("Tanii 10n jiliin daraah nas " +(nowAge+10));
+
+
+
+var eh,hy,gu,ha;
+eh=3;
+hy=4;
+gu=5;
+ha=eh+hy+gu;
+console.log(eh+hy+gu);
+console.log(ha);
